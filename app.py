ee226c9f892f181d03e1e8bb4d4580bfb73336e9
Brendanlearnings <brendanlearnings@gmail.com>
2023-03-16 15:22:12 +0200

    Yes

--- app.py
+++ app.py
@@ -3,9 +3,23 @@ import pandas as pd
 import streamlit as st
 import streamlit.components.v1 as components
 ############################################
+def interactive_map():
+    st.title('Interactive Map')
+    st.write('Feel free to play around with the map and draw whatever features you would like to draw')
+    interactive_map = open('interactive_map.html','r')
+    html = interactive_map.read()
+    components.html(html, height = 600)
 
-interactive_map = open('interactive_map.html','r')
-html = interactive_map.read()
-components.html(html, height = 600)
+def test_page():
+    st.write('This is a test page')
+# Set up the directory for pages in app
+pages = {
+    "Interactive Map": interactive_map,
+    "TEST": test_page
+}
 
-st.write('Test')
+# Create a menu with the page names
+selection = st.sidebar.radio("Go to", list(pages.keys()))
+
+# Display the selected page with its corresponding function
+pages[selection]()
