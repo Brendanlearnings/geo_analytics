01b96f0600c33eee763cd40134b9163a032e69a2
Brendanlearnings <brendanlearnings@gmail.com>
2023-03-24 13:56:41 +0200

    uPDATES

--- app.py
+++ app.py
@@ -3,20 +3,8 @@ import pandas as pd
 import streamlit as st
 import streamlit.components.v1 as components
 ############################################
+
 def interactive_map():
-    st.title('Interactive Map')
-    st.write('Feel free to play around with the map and draw whatever features you would like to draw')
-    interactive_map = open('interactive_map.html','r')
-    html = interactive_map.read()
-    components.html(html, height = 600)
-
-def test_page():
-    st.write('This is a test page')
-    download = open('test_download.html','r')
-    html = download.read()
-    components.html(html)
-
-def test_download():
     st.title('Testing the download functionality after making a selection')
     maps = open('updated.html','r+')
     html = maps.read()
@@ -24,9 +12,7 @@ def test_download():
 
 # Set up the directory for pages in app
 pages = {
-    "Interactive Map": interactive_map,
-    "TEST": test_page,
-    "Updated":test_download
+    "Interactive Map": interactive_map
 }
 
 # Create a menu with the page names
