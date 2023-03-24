24ef0d50be76f3fbc5dd370404f99fe88e8c17ae
Brendanlearnings <brendanlearnings@gmail.com>
2023-03-24 14:58:24 +0200

    update to use json package

--- geocode.py
+++ geocode.py
@@ -1,4 +1,5 @@
 from opencage.geocoder import OpenCageGeocode
+import json
 import datetime 
 
 # API key value 
@@ -6,7 +7,11 @@ key = "c4b6309106ef4859ae4c30d240067958"
 geocoder = OpenCageGeocode(key)
 
 def geocode(location):
-    geocoding_lat = geocoder.geocode(location)['geometry']['lat']
-    geocoding_long = geocoder.geocode(location)['geometry']['lng']
+    request = geocoder.geocode(location)
+
+    json_dict = json.loads(request)
+
+    geocoding_lat = geocoder.geocode(json_dict)['geometry']['lat']
+    geocoding_long = geocoder.geocode(json_dict)['geometry']['lng']
 
     return [geocoding_lat,geocoding_long]
