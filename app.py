3dda4db27c8076172114747f90c43990756930d4
Brendanlearnings <brendanlearnings@gmail.com>
2023-03-24 14:01:54 +0200

    Updated text

--- app.py
+++ app.py
@@ -5,10 +5,11 @@ import streamlit.components.v1 as components
 ############################################
 
 def interactive_map():
-    st.title('Testing the download functionality after making a selection')
+    st.title('Point of Interest within a given isochrone')
+    st.write('This interactive map allows you to draw features and see what points of interest fall within the given polygon, feel free to download the features contained in the polygon once you are done!')
     maps = open('updated.html','r+')
     html = maps.read()
-    components.html(html, height = 900)
+    components.html(html, height = 600)
 
 # Set up the directory for pages in app
 pages = {
