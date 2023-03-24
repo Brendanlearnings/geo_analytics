8ea3126ed7050afcc7e728f46ad4273cd1f60950
Brendanlearnings <brendanlearnings@gmail.com>
2023-03-24 14:51:00 +0200

    updated geocode

--- geocode.py
+++ geocode.py
@@ -6,7 +6,7 @@ key = "5e28e026c47a478384384342fb05f229"
 geocoder = OpenCageGeocode(key)
 
 def geocode(location):
-    geocoding_lat = geocoder.geocode(location[0], no_annotations="1")[0]['geometry']['lat']
-    geocoding_long = geocoder.geocode(location[0], no_annotations="1")[0]['geometry']['lng']
+    geocoding_lat = geocoder.geocode(location, no_annotations="1")[0]['geometry']['lat']
+    geocoding_long = geocoder.geocode(location, no_annotations="1")[0]['geometry']['lng']
 
     return [geocoding_lat,geocoding_long]
