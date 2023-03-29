6a50bbd6a3dee45ad49ca2d7030ba3a60255817d
Brendanlearnings <brendanlearnings@gmail.com>
2023-03-29 11:02:06 +0200

    Updated pip packages

--- geocode.py
+++ geocode.py
@@ -1,13 +1,8 @@
 from opencage.geocoder import OpenCageGeocode
-import json
-import datetime 
 
 # API key value 
 key = "c4b6309106ef4859ae4c30d240067958"
 geocoder = OpenCageGeocode(key)
-
-#request = geocoder.geocode('2 Clam Road, Tableview, Cape Town')
-#print(request)
 
 def geocode(location):
     
