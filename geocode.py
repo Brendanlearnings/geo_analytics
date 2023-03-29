8e18e3a0d8b7a57b562c37753e10ef5bd14e7ecf
Brendanlearnings <brendanlearnings@gmail.com>
2023-03-29 14:59:31 +0200

    Update to dataframe

--- geocode.py
+++ geocode.py
@@ -1,16 +1,16 @@
 from opencage.geocoder import OpenCageGeocode
+import pandas as pd 
 
 # API key value 
 key = "c4b6309106ef4859ae4c30d240067958"
 geocoder = OpenCageGeocode(key)
 
-def geocode(location):
-    
-    request = geocoder.geocode(location)
-    request = request[0]
-    
-    lat = request['geometry']['lat']
-    long = request['geometry']['lng']
 
-    return [lat,long]
+def geocode(location):
+
+    request = geocoder.geocode(location)[0]
+    
+    df = pd.DataFrame(request)
+
+    return df
 
