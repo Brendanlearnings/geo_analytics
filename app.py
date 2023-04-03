878172d1bc09a64915bc78f8bd0d3b36c9e755ba
Brendanlearnings <brendanlearnings@gmail.com>
2023-04-03 16:20:50 +0200

    should work

--- app.py
+++ app.py
@@ -106,9 +106,9 @@ def route_matrix():
             traf = 'false'
 
         route_plan = rp.route_matrix(points=geocoded_points,avoid=[],departAt=None,RouteType=route,travelMode=vehicle,traffic=traf)
-        route_plan = route_plan[0]
+        route_pointz = route_plan[0]
         st.write(route_plan[1])
-        route_data = route_plan["routes"][0]["legs"][0]["points"]
+        route_data = route_pointz["routes"][0]["legs"][0]["points"]
         route_points = pd.json_normalize(route_data)
         st.map(route_points)
         
