15974dc0e1d43fec87b700c92061bd1baf72825d
Brendanlearnings <brendanlearnings@gmail.com>
2023-04-03 11:34:56 +0200

    remove label visibility out of text_input

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def geocode():
     # Display elements
     st.title('Geocode an address or point of interest to a extract some usefull information from it.')
 
-    address = st.text_input(value='4 Waterfall Street, Century City, Cape Town', label_visibility='hidden')
+    address = st.text_input(value='4 Waterfall Street, Century City, Cape Town')
     point_on_map = geocode_to_df(address)[0]
     
     st.map(point_on_map)
