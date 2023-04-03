bbffccfa9a5a36f41bcefaf018a8ad9d993e63e2
Brendanlearnings <brendanlearnings@gmail.com>
2023-04-03 15:18:07 +0200

    speeling error

--- route_planner.py
+++ route_planner.py
@@ -15,7 +15,7 @@ def route_matrix(points:list = [], avoid:list = [], departAt:str = None, compute
         depart_at = f'{depart_at}'
 
     # Construct the Rest API request
-    querystring = {'api-version':'1.0',"subscription-key":"gOpOAK_LpXt8qCWYwUwp9JRrscQ-xnrnpCtexOqaGS4",'query':f'{flattened_coords}','avoid':f'{aviods_poi}','departAt':f'{depart_at}','computeBestOrder':f'{compute_best_order}','computeTravelTimeFor':f'{computeTravelTimeFor}','routeRepresentation':f'{RouteRepresentationForBestOrder}','routeType':f'{routeType}','travelMode':f'{travelMode}','traffic':f'{traffic}'}
+    querystring = {'api-version':'1.0',"subscription-key":"gOpOAK_LpXt8qCWYwUwp9JRrscQ-xnrnpCtexOqaGS4",'query':f'{flattened_coords}','avoid':f'{aviods_poi}','departAt':f'{depart_at}','computeBestOrder':f'{compute_best_order}','computeTravelTimeFor':f'{computeTravelTimeFor}','routeRepresentation':f'{RouteRepresentationForBestOrder}','routeType':f'{RouteType}','travelMode':f'{travelMode}','traffic':f'{traffic}'}
 
     payload = ""
     response = requests.request("GET", url, data=payload, params=querystring).json()
