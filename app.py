20ed4fe1d829cabcb17496a34203217d7bde4e87
Brendanlearnings <brendanlearnings@gmail.com>
2023-04-03 11:53:39 +0200

    to list

--- app.py
+++ app.py
@@ -48,10 +48,9 @@ def route_matrix():
     st.experimental_data_editor(construct_df,num_rows='dynamic',key="data_editor")
 
     if st.button('Submit'):
-        data_for_request = construct_df.to_dict()
-        st.write(data_for_request)
-
+        data_for_request = construct_df.to_list()
         st.write(st.session_state["data_editor"])
+
 
 
 #         construct_df['Lattitude'] = geocode_to_df(construct_df['Address'])[2]
