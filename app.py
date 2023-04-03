f48d53d6f7580483c923b8afd119847cd617e7b7
Brendanlearnings <brendanlearnings@gmail.com>
2023-04-03 12:06:22 +0200

    update

--- app.py
+++ app.py
@@ -52,8 +52,8 @@ def route_matrix():
         dict_data = st.session_state["data_editor"]
         for address in dict_data['added_rows']:
             st.write(address)
-            adrss = address[0]
-            st.write(adrss)
+            # adrss = address[0]
+            # st.write(adrss)
         
 
 
