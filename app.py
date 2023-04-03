71acc346737c9d596f18eca42ff1524d9e629c89
Brendanlearnings <brendanlearnings@gmail.com>
2023-04-03 12:46:18 +0200

    lol df not list

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ def route_matrix():
                 }
 
                 insert_df = pd.DataFrame(construct_data)
-                output_df = pd.concat(construct_data,insert_df,ignore_index=True)
+                output_df = pd.concat(output_df,insert_df,ignore_index=True)
 
         st.dataframe(output_df)
         st.map(output_df)
