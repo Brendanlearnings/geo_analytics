b33d11be2170074bcf5b654499517486660dc35a
Brendanlearnings <brendanlearnings@gmail.com>
2023-04-04 14:47:44 +0200

    woops

--- app.py
+++ app.py
@@ -167,7 +167,7 @@ def route_matrix():
         st.json(data_points_for_route)
         maps_df = pd.json_normalize(data_points_for_route)
         st.dataframe(maps_df)
-        st.pydeck_chart(pdk.Deck(map_style=None,initial_view_state=(latitude=-34.11818,longitude=18.83057,zoom=10)),
+        st.pydeck_chart(pdk.Deck(map_style=None,initial_view_state=pdk.ViewState(latitude=-34.11818,longitude=18.83057,zoom=10)),
                         layers = [pdk.Layer(
                         type="PathLayer",
                         data=maps_df,
