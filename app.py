9436463de578d79ae1f9113f0612fe6fc6ccb6e7
Brendanlearnings <brendanlearnings@gmail.com>
2023-04-04 14:16:58 +0200

    pls man

--- app.py
+++ app.py
@@ -133,7 +133,7 @@ def route_matrix():
         route_from_json = [points['points'] for points in route_plan['routes'][0]['legs']]
         for lat_long in route_from_json:
             for dict_obj in lat_long:
-                route_list.append(lat_long.values())
+                st.write(type(dict_obj))
         st.write(route_list)
         # for route in route_from_json:
         #     route_list.append(route.values())
