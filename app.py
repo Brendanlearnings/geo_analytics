37761a200d4d7c21717cf3ed6dcc6584f93e123c
Brendanlearnings <brendanlearnings@gmail.com>
2023-04-04 15:27:05 +0200

    test new map

--- app.py
+++ app.py
@@ -182,7 +182,42 @@ def route_matrix():
                         initial_view_state=state_view,
                         tooltip={"text": "{name}"}
                         ))
- 
+
+def test_map():
+
+
+    chart_data = pd.DataFrame(
+       np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+       columns=['lat', 'lon'])
+    
+    st.pydeck_chart(pdk.Deck(
+        map_style=None,
+        initial_view_state=pdk.ViewState(
+            latitude=37.76,
+            longitude=-122.4,
+            zoom=11,
+            pitch=50,
+        ),
+        layers=[
+            pdk.Layer(
+               'HexagonLayer',
+               data=chart_data,
+               get_position='[lon, lat]',
+               radius=200,
+               elevation_scale=4,
+               elevation_range=[0, 1000],
+               pickable=True,
+               extruded=True,
+            ),
+            pdk.Layer(
+                'ScatterplotLayer',
+                data=chart_data,
+                get_position='[lon, lat]',
+                get_color='[200, 30, 0, 160]',
+                get_radius=200,
+            ),
+        ],
+    ))
         # pydeck_obj = pdk.Deck(layers=[layer], initial_view_state=view_state, tooltip={"text": "{name}"})
         # components.html(pydeck_obj, height = 700)
 
@@ -221,7 +256,8 @@ def route_matrix():
 pages = {
     "Interactive Map": interactive_map,
     "Geocode": geocode,
-    "Route Optimization": route_matrix
+    "Route Optimization": route_matrix,
+    "testMap": test_map
 }
 
 # Create a menu with the page names
