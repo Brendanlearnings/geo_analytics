e47df54a204421f2610aec966ace53ec7a6c05a3
Brendanlearnings <brendanlearnings@gmail.com>
2023-04-04 12:32:01 +0200

    I think this will work

--- app.py
+++ app.py
@@ -136,7 +136,7 @@ def route_matrix():
             name = address
             data = {'name':[f"{address_list[address]} - {address_list[address+1]}"],
                     'color':[random_color_generator()],
-                    'path':[]}
+                    'path':[route_from_json[address]]}
             data_points_for_route.append(data)
             
         st.json(data_points_for_route)
