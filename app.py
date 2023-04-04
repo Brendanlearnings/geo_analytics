64a8a69138c440b2bb8797cb276b84fbafd9ea4c
Brendanlearnings <brendanlearnings@gmail.com>
2023-04-04 14:13:57 +0200

    pllelle

--- app.py
+++ app.py
@@ -130,9 +130,10 @@ def route_matrix():
         data_points_for_route = []
         address_list = output_df['Address'].values.tolist()
         route_list = []
-        route_from_json = [dict(points['points']).values() for points in route_plan['routes'][0]['legs']]
-
-        st.write(route_from_json)
+        route_from_json = [points['points'] for points in route_plan['routes'][0]['legs']]
+        for lat_long in route_from_json:
+            route_list.append(lat_long.values())
+        st.write(route_list)
         # for route in route_from_json:
         #     route_list.append(route.values())
          
