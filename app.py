261a0c60340f30d4c1444084f279e2249297c4ee
Brendanlearnings <brendanlearnings@gmail.com>
2023-04-04 15:36:25 +0200

    test demo script

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import route_planner as rp
 import pydeck as pdk
 import random
 import numpy as np
+import json
 ############################################
 
 ####### Fucntions for reuse ####
@@ -185,38 +186,38 @@ def route_matrix():
 def test_map():
 
 
-    chart_data = pd.DataFrame(
-       np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-       columns=['lat', 'lon'])
+    DATA_URL = "https://raw.githubusercontent.com/visgl/deck.gl-data/master/website/bart-lines.json"
+    df = pd.read_json(DATA_URL)
+
+
+    def hex_to_rgb(h):
+        h = h.lstrip("#")
+        return tuple(int(h[i : i + 2], 16) for i in (0, 2, 4))
+
+
+    df["color"] = df["color"].apply(hex_to_rgb)
+
+
+    view_state = pdk.ViewState(latitude=37.782556, longitude=-122.3484867, zoom=10)
+
+    layer = pdk.Layer(
+        type="PathLayer",
+        data=df,
+        pickable=True,
+        get_color="color",
+        width_scale=20,
+        width_min_pixels=2,
+        get_path="path",
+        get_width=5,
+    )
 
     st.pydeck_chart(pdk.Deck(
-        map_style=None,
-        initial_view_state=pdk.ViewState(
-            latitude=37.76,
-            longitude=-122.4,
-            zoom=11,
-            pitch=50,
-        ),
-        layers=[
-            pdk.Layer(
-               'HexagonLayer',
-               data=chart_data,
-               get_position='[lon, lat]',
-               radius=200,
-               elevation_scale=4,
-               elevation_range=[0, 1000],
-               pickable=True,
-               extruded=True,
-            ),
-            pdk.Layer(
-                'ScatterplotLayer',
-                data=chart_data,
-                get_position='[lon, lat]',
-                get_color='[200, 30, 0, 160]',
-                get_radius=200,
-            ),
-        ],
-    ))
+        layers = [layer],
+        initial_view_state = view_state,
+        tooltip =tooltip={"text": "{name}"}
+    )
+    )
+
         # pydeck_obj = pdk.Deck(layers=[layer], initial_view_state=view_state, tooltip={"text": "{name}"})
         # components.html(pydeck_obj, height = 700)
 
