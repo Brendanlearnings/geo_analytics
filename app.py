6d1f8d71b40df01e7d49267c074df7049e90664b
Brendanlearnings <brendanlearnings@gmail.com>
2023-04-06 13:48:51 +0200

    whyyyy

--- app.py
+++ app.py
@@ -206,6 +206,9 @@ def route_matrix():
         df["color"] = df["color"].apply(hex_to_rgb)
         view_state = pdk.ViewState(latitude=-33.743, longitude=18.96527, zoom=10)
 
+        df2["color"] = df2["color"].apply(hex_to_rgb)
+        view_state2 = pdk.ViewState(latitude=-33.743, longitude=18.96527, zoom=10)
+
         layer = pdk.Layer(
             type="PathLayer",
             data=df,
@@ -219,6 +222,21 @@ def route_matrix():
 
         st.pydeck_chart(pdk.Deck(
             layers=[layer], initial_view_state=view_state, tooltip={"text": "{name}"}
+        ))
+
+        layer2 = pdk.Layer(
+            type="PathLayer",
+            data=df2,
+            pickable=True,
+            get_color="color",
+            width_scale=20,
+            width_min_pixels=2,
+            get_path="path",
+            get_width=5,
+        )
+
+        st.pydeck_chart(pdk.Deck(
+            layers=[layer2], initial_view_state=view_state2, tooltip={"text": "{name}"}
         ))
         ##st.dataframe(maps_df)
         #layer = pdk.Layer(
